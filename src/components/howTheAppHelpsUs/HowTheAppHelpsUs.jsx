4994d3cb4a7eb5d9a09fbe81371bb68caff68e57
RoiHendler <73064092+RoiHendler@users.noreply.github.com>
2022-06-21 19:03:19 +0300

    remove icon's

--- src/components/howTheAppHelpsUs/HowTheAppHelpsUs.jsx
+++ src/components/howTheAppHelpsUs/HowTheAppHelpsUs.jsx
@@ -5,7 +5,6 @@ export default function HowTheAppHelpsUs() {
     {
       id: 1,
       img: "assets/problemManIcon.png",
-      // icon: "assets/youtube.png",
       desc:
         "Until today, there was no database that combines all the swimmers results. Each coach kept and organized the information independently. It was difficult to analyze the information that each coach kept",
 
@@ -14,7 +13,6 @@ export default function HowTheAppHelpsUs() {
     {
       id: 2,
       img: "assets/selvpro.jpg",
-      // icon: "assets/youtube.png",
       desc:
         "The app saves the information and analyzes the swimmers results automatically, making the information accessible to swimmers, coaches and even the swimmers parents. ",
       featured: true,
@@ -23,8 +21,6 @@ export default function HowTheAppHelpsUs() {
     {
       id: 3,
       img: "assets/problem-solving.png",
-
-      // icon: "assets/linkedin.png",
       desc: 
       "In addition, the app has a message board of the swiming association and the swimming group so that all the information is organized in one place. In addition, in the app has a library of instructional videos that include instruction in dry exercises and even exercises to improve swimming style.",
 
@@ -49,10 +45,6 @@ export default function HowTheAppHelpsUs() {
             <h4>{d.desc}</h4>
             </div>
             <div className="bottom">
-
-              {/* <h3>{d.name}</h3> */}
-              {/* <h4>{d.desc}</h4> */}
-
             </div>
           </div>
         ))}
